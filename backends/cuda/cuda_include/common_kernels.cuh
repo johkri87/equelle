fb651f79ca806c6d70b36048efb820fc98aa8f5c
johkri87 <johkri87@gmail.com>
2018-07-27 02:56:13 +0200

    Added new files for generic CUDA kernels.

---
+++ backends/cuda/cuda_include/common_kernels.cuh
@@ -0,0 +1,18 @@
+#ifndef EQUELLE_KERNELS_HEADER_INCLUDED
+#define EQUELLE_KERNELS_HEADER_INCLUDED
+namespace equelleCUDA {
+namespace EquelleKernels {
+__global__ void multiplication_kernel(double* out, const double* rhs, const int size);
+__global__ void multiplication_kernel(double* out, const double rhs, const int size);
+__global__ void addition_kernel(double* out, const double* rhs, const int size);
+__global__ void division_kernel(double* out, const double* rhs, const int size);
+__global__ void division_kernel(double* out, const double rhs, const int size);
+__global__ void division_kernel(const double lhs, double* out, const int size);
+__global__ void negate_kernel(double* out, const int size);
+__global__ void square_kernel(double* out, const int size);
+__global__ void sqrt_kernel(double* out, const int size);
+__global__ void reciprocal_kernel(double* out, const int size);
+__global__ void abs_kernel(double* out, const int size);
+}
+}
+#endif // EQUELLE_KERNELS_HEADER_INCLUDED
